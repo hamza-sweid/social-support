5429197c35ecd78d60300ff4dd235094d11f55b7
Hamza <hamza.sweid963@gmail.com>
2025-10-02 01:58:59 +0400

    FEATURE: implement progress with the form steps

--- src/components/steps/StepProgress.tsx
+++ src/components/steps/StepProgress.tsx
@@ -1,57 +1,25 @@
-import { useState } from 'react';
-import { Button, message, Steps, theme } from 'antd';
+import { Steps } from 'antd';
 import './StepProgress.module.scss';
+import { useTranslation } from 'react-i18next';
 
 const steps = [
-  { title: 'First', content: 'First-content' },
-  { title: 'Second', content: 'Second-content' },
-  { title: 'Last', content: 'Last-content' },
+  { title: 'personalInfo', content: 'First-content' },
+  { title: 'familyInfo', content: 'Second-content' },
+  { title: 'situationDescription', content: 'Last-content' },
 ];
 
-const StepProgress = () => {
-  const { token } = theme.useToken();
-  const [current, setCurrent] = useState(0);
+const StepProgress = ({ currentStep }: { currentStep: number }) => {
+  const { t } = useTranslation();
 
-  const next = () => setCurrent(current + 1);
-  const prev = () => setCurrent(current - 1);
-
-  const items = steps.map((item) => ({ key: item.title, title: item.title }));
+  const items = steps.map((item) => ({
+    key: item.title,
+    title: t(`applicationForm.steps.${item.title}`),
+  }));
 
   return (
     <>
-      <Steps current={current} items={items} />
-      <div
-        className="step-content"
-        style={{
-          backgroundColor: token.colorFillAlter,
-          borderRadius: token.borderRadiusLG,
-          border: `1px dashed ${token.colorBorder}`,
-          color: token.colorTextTertiary,
-        }}
-      >
-        {steps[current].content}
-      </div>
-
-      <div className="step-buttons">
-        {current > 0 && (
-          <Button className="prev-btn" onClick={prev}>
-            Previous
-          </Button>
-        )}
-        {current < steps.length - 1 && (
-          <Button type="primary" onClick={next}>
-            Next
-          </Button>
-        )}
-        {current === steps.length - 1 && (
-          <Button
-            type="primary"
-            onClick={() => message.success('Processing complete!')}
-          >
-            Done
-          </Button>
-        )}
-      </div>
+      <Steps responsive current={currentStep - 1} items={items} />
+      <div className="step-content"></div>
     </>
   );
 };
--- src/pages/application-form/Index.tsx
+++ src/pages/application-form/Index.tsx
@@ -4,19 +4,20 @@ import styles from './Index.module.scss';
 import { FamilyInfo } from './components/family-info/Index';
 import { SituationDescription } from './components/situation-info/Index';
 import { useTranslation } from 'react-i18next';
+import StepProgress from '../../components/steps/StepProgress';
 
 const ApplicationForm = () => {
   const { t } = useTranslation();
   const [step, setStep] = useState(1);
 
   const handleNext = () => {
-    console.log('Next step');
     setStep((prev) => prev + 1);
   };
 
   return (
     <div className={styles.container}>
       <h1>{t('applicationForm.title')}</h1>
+      <StepProgress currentStep={step} />
       <div className={styles.stepWrapper}>
         {step === 1 && <PersonalInfo onNext={handleNext} />}
         {step === 2 && (
